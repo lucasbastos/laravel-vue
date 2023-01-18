71613ee5aa994f5a98f735a77fbbadbfcbf9c165
Lucas Bastos <lucasbastos.ecomp@gmail.com>
2023-01-18 01:03:48 -0300

    first commit

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
